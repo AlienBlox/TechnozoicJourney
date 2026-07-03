4a5da9b3b7d62c5aeeb13b2e097702b6d5d28a16
AlienBlox <alienthebest123@outlook.com>
2026-07-03 15:34:49 +0100

    lore

---
+++ Content/Items/TreasureBags/StarterBox.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TechnozoicJourney.Content.Items.TreasureBags
+{
+    public class StarterBox : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Content.Items.TreasureBags";
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 16;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.sellPrice();
+        }
+
+        public override bool CanRightClick()
+        {
+            return true;
+        }
+
+        public override void ModifyItemLoot(ItemLoot itemLoot)
+        {
+            
+        }
+    }
+}
